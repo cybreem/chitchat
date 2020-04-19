e00b7626e50da8e4c55be5a4c7a9299a469d92f1
diankhismaw <diankhismaw@gmail.com>
2020-04-19 21:22:35 +0700

    perubahan welocome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,8 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Home Test update</a>
+                        <a href="{{ url('/home') }}">Home Test update v2</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
